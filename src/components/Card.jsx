e0cad40c939d4d2803ada1b5b7b9f00fbccd67d3
motajessica <motajessicaa@gmail.com>
2023-10-30 14:38:12 +1300

    Fix styles for the flipped card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,16 @@ function Card({ image, isFlipped, onClick }) {
         width: '200px',
         height: '150px',
         background: isFlipped ? `url(${image})` : 'purple',
-        cursor: 'pointer',
-        backgroundSize: 'contain',
+        cursor: 'pointer' ,
         margin: '40px',
-        
-        
     };
-
+    
     return (
         <div style={cardStyle} onClick={onClick}>
+            {isFlipped && <img src={image} alt="Memory card" style={{width: '100%', height: '100%', objectFit: 'cover'}} />}
         </div>
     );
+    
 }
 
 export default Card;
