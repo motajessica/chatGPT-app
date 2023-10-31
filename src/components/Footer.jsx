ce383cb57ba8cc111cbfddfe0fcc703e8c2d8dd6
motajessica <motajessicaa@gmail.com>
2023-10-31 19:25:37 +1300

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div>
       <footer className="footer">
             <p>Discover the beauty of New Zealand's sea life.</p>
+            <p> © {currentYear}</p>
         </footer>
     </div>
   )
