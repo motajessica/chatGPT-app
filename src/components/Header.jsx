4f6341b52050f0465e4bbc28b9d638c75765674b
motajessica <motajessicaa@gmail.com>
2023-11-06 12:11:34 +1300

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div>
       <header className="header">
-        <h1>New Zealand Ocean Creatures</h1>
-        <h4>Meet Our Marine Mates and Their Watery Worlds</h4>
+        <h1 className="title">Aotearoa Sea Life</h1>
+        <h4 className="sub-title">Meet our Marine Mates and Their Watery Worlds</h4>
       </header>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,18 +5,58 @@ export const CHARACTERS = [
       "https://images.theconversation.com/files/405667/original/file-20210610-19-19zcvem.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C4437%2C2955&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
   },
   {
+    name: "Blue Penguin",
+    image:
+      "https://nzbirdsonline.org.nz/sites/all/files/1200121BluePenguin1.jpg",
+  },
+  {
     name: "Hector Dolphin",
     image:
       "https://missionblue.org/wp-content/uploads/2016/05/face-on-jump-4.jpg",
+  },
+  {
+    name: "Orca",
+    image: "https://teara.govt.nz/files/p-3999-iv.jpg",
+  },
+
+  {
+    name: "Humpback Whale",
+    image:
+      "https://www.antarctica.gov.au/site/assets/files/45693/ia40278.1600x0.jpeg",
+  },
+  {
+    name: "Southern Right Whale",
+    image:
+      "https://i.guim.co.uk/img/media/e9dc88702dbb903316cc339d9c7cad4c58030bde/0_458_6870_4122/master/6870.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=9dc2888cb61c1cc9fb815acc9be6c44d",
   },
   {
     name: "Fur Seal",
     image:
       "https://www.doc.govt.nz/globalassets/images/nature/native-animals/marine-mammals/seals/nz-fur-seal/furseal-kclements-1200-12.jpg",
   },
+
   {
-    name: "Blue Penguin",
+    name: "Eels",
     image:
-      "https://nzbirdsonline.org.nz/sites/all/files/1200121BluePenguin1.jpg",
+      "https://cdn.aucklandunlimited.com/zoo/assets/media/long-fin-eel-gallery-1.jpg",
+  },
+  {
+    name: "Giant Manta-Ray",
+    image:
+      "https://www.doc.govt.nz/thumbs/hero/contentassets/30c77019fd574ba1abbd388bc8774ff1/littlebarrier-giant-manta.jpg",
+  },
+  {
+    name: "Spiny Sea Dragons ",
+    image:
+      "https://fishesofaustralia.net.au/Images/Image/SolegnathusSpinossissimus4PRyan.jpg",
+  },
+  {
+    name: "Greenshell Mussell",
+    image: "https://www.aotea.com/images/genryo_biolane.jpg",
+  },
+  {
+    name: "Pipi",
+    image:
+      "https://www.fishing.net.nz/default/assets/Image/2019/nz-shellfish-7.jpg",
   },
 ];
